8b7561ee421474b63e7f786860ac91faeceadffd
nick <nick.zwart@gmail.com>
2015-11-03 12:54:31 -0700

    Removed c-sort utils build.

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# Brief: For building "the Decider" C routine and Qt4 UI
+# Brief: For building Qt4 UI
 
 set -x
 set -e
-
-## C EXT MODULE
-# build the extension module for sorting
-python decutil_setup.py build_ext --inplace
 
 ## QT4 UI (C to PYTHON)
 # osx
