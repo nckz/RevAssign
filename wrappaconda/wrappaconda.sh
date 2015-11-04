83d189663bc8a462b33c79ca1c387c99bd810c06
nick <nick.zwart@gmail.com>
2015-11-04 15:56:57 -0700

    Switch to wrappaconda in the PATH.

--- wrappaconda/wrappaconda.sh
+++ wrappaconda/wrappaconda.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # wrap into an app using wrappaconda
-WRAPPACONDA=$1
-$WRAPPACONDA -n RevAssign -t revassign -o --py2 -c nckz -p revassign -i ../icon.icns
+wrappaconda -n RevAssign -t revassign -o --py2 -c nckz -p revassign -i ../icon.icns
